d42331fc301cee4d9a1293c074970329ca7d2c7e
killerMert <mert_3377@hotmail.com>
2016-01-15 16:26:55 +0100

    asd

---
+++ test_installation.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+#-------------------------------------------------------------
+#     FILE: TEST_INSTALLATION.BASH
+#     DESCRIPTION: TOOLS THAT WILL BE USED FOR TESTING IN THE ENVIRONMENT
+#     VERSION: 1.0
+#-------------------------------------------------------------
+
+#first cloning the repository for code
+git clone feature-01 http://github.com/SDEV56/INFPRJ0156.git
+
+
+#Installing Cppunit
+apt-get install libcppunit-doc libcppunit-dev
+
+#installing cloc
+sudo apt-get install cloc
+echo 'Installation is complete..'
+echo 'Running cloc..'
+
+
+#make directory for test logs
+mkdr logs
+
+#running cloc
+cat logs/cloc_log.txt
+cloc --out=logs/cloc_log.txt INFPRJ0156
+
+
--- tool_installation.bash
+++ tool_installation.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #-------------------------------------------------------------
 #     FILE: TOOL_INSTALLATION.BASH
-#     DESCRIPTION: INSTALLATION OF TOOLS FOR ENVIROMENT
+#     DESCRIPTION: INSTALLATION OF TOOLS FOR ENVIRONMENT
 #     VERSION: 1.0
 #-------------------------------------------------------------
 
